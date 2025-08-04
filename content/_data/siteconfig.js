eae4013073c549f19855065c3fe2584436b7b344
Your Name <ruldak3306@gmail.com>
2025-08-04 20:20:37 +0700

    feat: replace dummy content with real content and update social links to actual profiles

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -18,34 +18,8 @@ module.exports = {
     // sharing.
     openGraphDefaultImage: "/assets/images/opengraph.jpg",
     // GitHub ID (optional, remove it not needed), used for link in the left sidebar
-    socialGitHub: "muenzpraeger",
+    socialGitHub: "ruldak",
     // LinkedIn ID  (optional, remove it not needed), used for link in the left sidebar
-    socialLinkedIn: "muenzpraeger",
-    // Twitter ID  (optional, remove it not needed), used for link in the left sidebar, and for OpenGraph sharing information
-    socialTwitter: "muenzpraeger",
-    // YouTube ID/Channel  (optional, remove it not needed), used for link in the left sidebar
-    socialYouTube: "UCH60RRaY2GI9m62z1loLjcA",
-    // Google Analytics ID  (optional, remove it not needed), used for... well, Google Analytics
-    googleAnalytics: "YOUR_GA-ID",
-    // Algolia-powered search  (optional, remove it not needed),
-    // See: https://github.com/algolia/algoliasearch-netlify
-    algoliaSearch: {
-        // When enabled shows the search bar in the UI
-        enabled: true,
-        // You'll have to set this manually in your build settings.
-        // The value comes from Algolia, and is either visible in the
-        // UI for the Crawler Plugin or the Algolia Dashboard.
-        appId: process.env.ALGOLIA_APP_ID,
-        // You'll have to set this manually in your build settings.
-        // The value comes from Algolia, and is either visible in the
-        // UI for the Crawler Plugin or the Algolia Dashboard.
-        searchApiKey: process.env.ALGOLIA_SEARCH_API_KEY,
-        // You'll have to set this manually in your build settings.
-        // The value comes from Algolia, and is either visible in the
-        // UI for the Crawler Plugin or the Algolia Dashboard.
-        siteId: process.env.ALGOLIA_SITE_ID,
-        // Assuming that you deploy your "main" branch. Otherwise you
-        // can either override or configure this (using process.env.HEAD)
-        branch: "main"
-    }
+    socialLinkedIn: "hary-cahyono-131a2536a",
+    googleAnalytics: "YOUR_GA-ID", 
 };
